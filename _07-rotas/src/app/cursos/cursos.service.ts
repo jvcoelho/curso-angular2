7862732884a3a530e507ab9b151625cbc1ad2add
João Coelho <jvcoelho@users.noreply.github.com>
2016-12-18 16:46:08 -0200

    Update cursos.service.ts

--- _07-rotas/src/app/cursos/cursos.service.ts
+++ _07-rotas/src/app/cursos/cursos.service.ts
@@ -14,7 +14,7 @@ export class CursosService {
     let cursos = this.getCursos();
     for (let i=0; i<cursos.length; i++){
       let curso = cursos[i];
-      if (curso.id == id){
+      if (curso.id === id){
         return curso;
       }
     }
